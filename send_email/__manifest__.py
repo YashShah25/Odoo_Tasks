e6d6cb30653bc5044e115141a12ea0e6e73c4829
Yash Shah <87748033+YashShah25@users.noreply.github.com>
2022-02-22 15:38:15 +0530

    Add files via upload

---
+++ send_email/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Product Email",
+    "version": "14.0.1.0.0",
+    "category": "Education",
+    "summary": "Send Email",
+    "sequence": 1,
+    "website": "https://www.aktivesoftware.com/",
+    "description": "",
+    "depends": ["mail",],
+    "data": [
+        "data/quotation_limit_mail_template_view.xml",
+        "views/products_view.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
